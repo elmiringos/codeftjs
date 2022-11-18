31cb9498012117264d4ba0cde5aa4cb11ed144a3
elmiringos <elmir.iskhakov1337@gmail.com>
2022-11-18 13:36:11 +0300

    add schema tokens

---
+++ model/token.js
@@ -0,0 +1,12 @@
+﻿const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  metamask_address: { type: String, required: true, unique: true },
+  token_address: { type: String, required: true, unique: true },
+  path_to_file: { type: String, required: true, unique: true }
+},
+{collection: 'tokens'})
+
+const model = mongoose.model('UserSchema', UserSchema)
+
+module.exports = model
